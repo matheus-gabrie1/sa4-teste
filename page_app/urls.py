993a039d505e31e6f14bbfd55d0b052e0bc08b33
matheus-gabrie1 <165507384+matheus-gabrie1@users.noreply.github.com>
2024-03-30 17:42:40 -0300

    projeto atualizado

--- page_app/urls.py
+++ page_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from page_app.views import index, contato
+from page_app.views import index, contato, services
 
 urlpatterns = [
     # Cadastrar URLs aqui
     path('', index),
 
     path('contato/', contato),
+
+    path('services/', services),
+
 ]
